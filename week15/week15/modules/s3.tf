f61c9578e307696b762eb9273479ac26ef206335
beebadeedoo <118843029+bebelo23@users.noreply.github.com>
2024-10-16 19:21:47 +0700

    Update s3.tf

--- week15/week15/modules/s3.tf
+++ week15/week15/modules/s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source            = "./modules/s3_bucket"
-  bucket_name       = "s3-bucket-127"     # เปลี่ยนชื่อนี้เป็นชื่อ bucket ที่คุณต้องการ
-  upload_file_source = "./my-melody.jpg" # path ไปยังไฟล์รูปภาพในเครื่องของคุณ
+  bucket_name       = "s3-bucket-127"     # เปลี่ยนชื่อนี้เป็นชื่อ bucket 
+  upload_file_source = "./my-melody.jpg" # path ไปยังไฟล์รูปภาพ
   upload_file_key    = "images/my_image.jpg"    # S3 key ที่ต้องการ (ไฟล์ path ใน S3)
 }
 
